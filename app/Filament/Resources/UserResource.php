019bc982d338f8c9791552c85a9d6cce0ad0b112
mmbehiclayton <mmbehiclayton@gmail.com>
2025-04-04 20:35:34 +0300

    initial commit

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->label('Name')
+                    ->placeholder('Enter your name'),
+                TextInput::make('email')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->label('Email')
+                    ->placeholder('Enter your email')
+                    ->email(),
+
+                DatePicker::make('email_verified_at')
+                    ->label('Email Verified At')
+                    ->placeholder('Select email verification date')
+                    ->default(now()),
+
+                TextInput::make('password')
+                    ->required()
+                    ->label('Password')
+                    ->placeholder('Enter your password')
+                    ->password()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn(Page $livewire) => $livewire instanceof CreateRecord),
+
+                TextInput::make('password_confirmation')
+                    ->required()
+                    ->label('Confirm Password')
+                    ->placeholder('Confirm your password')
+                    ->password()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn(Page $livewire) => $livewire instanceof CreateRecord),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email_verified_at')
+                    ->label('Email Verified At')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Updated At')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
+                
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
